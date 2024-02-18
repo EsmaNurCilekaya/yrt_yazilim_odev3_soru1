f29a70c01526afeb0617ef2d3add5cb187112ab7
EsmaNurCilekaya <157573081+EsmaNurCilekaya@users.noreply.github.com>
2024-02-19 02:27:14 +0300

    tanımlamalar

--- kod.c
+++ kod.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+int main(){
+	char kelime[6]={"roket"};
+	char tahmin[6];
+	char olmayan[21];
+	char hatalikonum[21];
+	int flag=1;
+	int i,a=0,b=0;
+  
+  return 0;
+}
